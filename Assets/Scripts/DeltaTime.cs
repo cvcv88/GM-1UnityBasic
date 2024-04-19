cae29012c9775afbbc754881c580deba91d2ca12
YJ <cvcv_cv@naver.com>
2024-04-19 17:53:54 +0900

    feat : B8 이동거리를 공평하게, 델타타임
    Time.deltaTime

---
+++ Assets/Scripts/DeltaTime.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeltaTime : MonoBehaviour
+{
+	// Time.deltaTime : 이전 프레임의 완료까지 걸린 시간
+     
+	/* Time.deltaTime 사용하는 방법
+	 * 
+     * Translate : 벡터에 곱하기
+     * translate.Translate(Vec1 * Time.deltaTime);
+     * 
+     * Vector 함수 : 시간 매개변수에 곱하기
+     * Vector3.Lerp(Vec2, Vec3, T * Time.deltaTime);
+     */
+}
